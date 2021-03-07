d7a1d7a2aa1eabe54ceb46ac421e4a4e54b6f52f
Cobular <22972550+JakeCover@users.noreply.github.com>
2021-03-07 15:21:57 -0800

    I think spell check works! Although it is a bit slow, I need to do another pass to optimize

--- Project4/Wurd/StudentSpellCheck.cpp
+++ Project4/Wurd/StudentSpellCheck.cpp
@@ -4,6 +4,7 @@
 #include <locale>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -30,22 +31,137 @@ bool StudentSpellCheck::load(std::string dictionaryFile)
 
 bool StudentSpellCheck::spellCheck(std::string word, int max_suggestions, std::vector<std::string>& suggestions)
 {
-    return false; // TODO
+    vector<string> m_suggestions;
+    const bool     spelledCorrect = correctWord(word, 0, max_suggestions, m_suggestions, m_trie->baseNode(), "");
+    // The word is spelled correct
+    if (spelledCorrect) { return true; }
+    // The word is not spelled correct, so we need to try to switch in our suggestions then return false.
+    // This applies even if our suggestions are empty, as we need to know if the word is spelled right either way.
+    vector<string> m_correctNumSuggestions = {
+        m_suggestions.begin(),
+        // If there are less suggestions than the max, return all. Otherwise, return the max number.
+        m_suggestions.size() < max_suggestions ? m_suggestions.end() : m_suggestions.begin() + max_suggestions
+    };
+    swap(m_correctNumSuggestions, suggestions);
+    return false;
 }
 
 void StudentSpellCheck::spellCheckLine(const std::string& line, std::vector<SpellCheck::Position>& problems)
 {
+    vector<SpellCheck::Position> words;
+    int                          startOfWord = 0;
+    int                          endOfWord   = 0;
+    for (int i = 0; i < line.size(); i++)
+    {
+        char clean_ch = cleanChar(line[i]);
+        // Char is clean, we're still in the middle of a word, so increment the end of the word
+        if (clean_ch != '\0') { endOfWord++; }
+        else
+            // Char is unclean, so we've ended a word. If end of word != start of word, we actually have a word, so spellcheck it and commit to the list. Either way, reset both to i
+        {
+            if (endOfWord - startOfWord > 0)
+            {
+                // Weird ternary operator here accounts for the space or other garbage character in front of words in the middle of sentances, but not at the start
+                if (!isWordSpelledCorrect(line.substr(startOfWord, endOfWord - startOfWord), m_trie->baseNode()))
+                {
+                    Position position = { startOfWord, endOfWord - 1 };
+                    problems.push_back(position);
+                }
+            }
+            endOfWord = i + 1;
+            startOfWord = i + 1;
+        }
+    }
+
+    // Need to run this one more time after the last word to finish it up
+    if (endOfWord - startOfWord > 0)
+    {
+        // Weird ternary operator here accounts for the space or other garbage character in front of words in the middle of sentances, but not at the start
+        if (!isWordSpelledCorrect(line.substr(startOfWord, endOfWord - startOfWord), m_trie->baseNode()))
+        {
+            Position position = { startOfWord, endOfWord };
+            problems.push_back(position);
+        }
+    }
     // TODO
+}
+
+bool StudentSpellCheck::correctWord(
+    const std::string&       str, int                   pos, const int& max_suggestions, vector<string>& similarWords,
+    StudentSpellCheck::Node* currentNode, const string& path, bool      hasFoundDifference)
+{
+    // We've gone too far, need to abort or we'll overread
+    if (pos >= str.length())
+    {
+        // Check if we're at the end of a word. If so, we've either found our word and can return true or we've found a spelling suggestion 
+        if (currentNode->findChild('.') != nullptr)
+        {
+            if (!hasFoundDifference) { return true; }
+            similarWords.push_back(path);
+            return false;
+        }
+        // If we're not at the end of a word, then we haven't found an alternate word so we can just return false
+        return false;
+    }
+
+    bool status = false;
+
+    // Check the children of this node
+    char ch = cleanChar(str[pos]);
+    // Somehow, we got a bad char in the string. Say something!
+    if (ch == '\0') cerr << "Managed to get a bad char in the spellcheck thing. Oopsie!" << endl;
+    Node* nextNode = currentNode->findChild(ch);
+    // Child node exists with the correct position, so just go there
+    if (nextNode != nullptr)
+    {
+        if (correctWord(str, pos + 1, max_suggestions, similarWords, nextNode, path + str[pos], hasFoundDifference))
+            status = true;
+    }
+    // If we haven't found our difference yet, we should explore all other paths, so explore all children but mark that the difference has been found
+    if (!hasFoundDifference)
+    {
+        for (Node* node : currentNode->children())
+        {
+            if (correctWord(str, pos + 1, max_suggestions, similarWords, node, path + node->value(), true)) status =
+                true;
+        }
+    }
+    // Child node doesn't exist and we have spent our one difference, so just give up on this branch
+    return status;
+}
+
+bool StudentSpellCheck::isWordSpelledCorrect(const std::string& str, Node* currentNode, int pos)
+{
+    // We've gone too far, need to abort or we'll overread
+    if (pos >= str.length())
+    {
+        // If the next char is the end word char, we've found our word! return true
+        if (currentNode->findChild('.') != nullptr) { return true; }
+            // Otherwise, we've missed the word and can return false
+        else { return false; }
+    }
+
+    bool status = false;
+
+    // Check the children of this node
+    char ch = cleanChar(str[pos]);
+    // Somehow, we got a bad char in the string. Say something!
+    if (ch == '\0') cerr << "Managed to get a bad char in the spellcheck thing. Oopsie!" << endl;
+    Node* nextNode = currentNode->findChild(ch);
+    // Child node exists with the correct position, so follow that
+    if (nextNode != nullptr) { return isWordSpelledCorrect(str, nextNode, pos + 1); }
+
+    // If not, we don't have a match, return false.
 }
 
 /// <summary>
 /// Normalized a character, nulling it if it's invalid
 /// </summary>
 /// <param name="ch">The character to check</param>
-/// <returns>The lowercase version of the char, a ', or the null char if the char is not alpha or '</returns>
+/// <returns>The lowercase version of the char, a ', or \0 if the char is not alpha or '</returns>
 char StudentSpellCheck::cleanChar(const char& ch)
 {
-    if (isalnum(ch) || ch == '\'') return tolower(ch);
+    if (isalpha(ch) || ch == '\'') return tolower(ch);
     return '\0';
 }
 
@@ -60,25 +176,39 @@ std::string StudentSpellCheck::cleanStr(const std::string& str)
     return outputStr;
 }
 
+StudentSpellCheck::Node* StudentSpellCheck::Node::findChild(char ch) const
+{
+    // Wacky lambda comparison to handle dereferencing the pointers in the Node* list.
+    const auto result = find_if(m_children.begin(), m_children.end(), [&ch](const Node* node)
+    {
+        return node->value() == ch;
+    });
+    if (result == m_children.end()) return nullptr;
+    return *result;
+}
+
 StudentSpellCheck::Trie::Trie() { m_base_node = new Node('\0'); }
 
 // Delete trie using a post-order traversal
 StudentSpellCheck::Trie::~Trie() { deleteNode(m_base_node); }
 
 // ReSharper disable once CppMemberFunctionMayBeConst
-void StudentSpellCheck::Trie::findOrCreateNodeForString(std::string str)
+void StudentSpellCheck::Trie::findOrCreateNodeForString(const std::string& str)
 {
-    Node* currentNode = m_base_node;
+    string cleanString = cleanStr(str);
+    cleanString += ".";
+    Node* currentNode = baseNode();
 
-    for (auto ch : str)
+    for (auto ch : cleanString)
     {
-        currentNode = currentNode->findChild(*str.begin());
-        if (currentNode == nullptr)
+        Node* checkNode = currentNode->findChild(ch);
+        if (checkNode == nullptr)
         {
-            Node* newNode = new Node(*str.begin());
+            Node* newNode = new Node(ch);
             currentNode->add_child(newNode);
             currentNode = newNode;
         }
+        else { currentNode = checkNode; }
     }
 }
 
--- Project4/Wurd/StudentSpellCheck.h
+++ Project4/Wurd/StudentSpellCheck.h
@@ -18,16 +18,42 @@ public:
 
 private:
     class Trie;
+    class Node;
+
+    /// <summary>
+    /// Spellchecks and corrects the given word
+    /// </summary>
+    /// <param name="str">The given word</param>
+    /// <param name="pos">The position, should be zero when called</param>
+    /// <param name="max_suggestions">The max num of suggestions to give. TODO: need to finish this part. </param>
+    /// <param name="similarWords">The vector through which words will be returned</param>
+    /// <param name="currentNode">The node to analyze the children of</param>
+    /// <param name="path">A string representing the path taken to this word.</param>
+    /// <param name="hasFoundDifference">If a difference has been found yet</param>
+    /// <returns>True if the word is found exactly and false otherwise</returns>
+    bool correctWord(
+        const std::string& str, int pos, const int& max_suggestions, std::vector<std::string>& similarWords,
+        Node*              currentNode, const std::string& path, bool hasFoundDifference = false);
+
+    /// <summary>
+    /// A simplified version of correctWord which just returns if a word exactly exists in the dictionary, without any suggestions. It's a good bit faster for sure. 
+    /// </summary>
+    /// <param name="str">The string to look for</param>
+    /// <param name="currentNode">The current node, feed in the base node. </param>
+    /// <param name="pos">The current position in the string, should be zero to start</param>
+    /// <returns>True if the word is found exactly and false otherwise</returns>
+    /// <returns>True if the word is found exactly and false otherwise</returns>
+    bool isWordSpelledCorrect(const std::string& str, Node* currentNode, int pos = 0);
 
     static char        cleanChar(const char& ch);
     static std::string cleanStr(const std::string& str);
 
+
     Trie* m_trie;
+
 
     class Node
     {
-        friend StudentSpellCheck;
-        friend Trie;
     public:
         Node(char ch) : m_value(ch) {}
         virtual            ~Node() = default;
@@ -47,13 +73,14 @@ private:
         std::vector<Node*> m_children;
     };
 
+
     class Trie
     {
-
     public:
         Trie();
         virtual ~Trie();
-        void    findOrCreateNodeForString(std::string str);
+        Node*   baseNode() const { return m_base_node; }
+        void    findOrCreateNodeForString(const std::string& str);
 
     private:
         static void deleteNode(Node* node);
